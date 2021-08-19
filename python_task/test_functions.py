16d3838baffaa78587a0bb28ffdd09e759564e39
Omar Magdy <contact.parmag@gmail.com>
2021-08-19 10:13:05 +0200

    test_functions.py:add:repeated lower

--- python_task/test_functions.py
+++ python_task/test_functions.py
@@ -12,13 +12,20 @@ pytest -rP
 pytest -rP --junitxml=test-reports/junit.xml --html=test-reports/pytest_report.html --self-contained-html
 """
 
-class CircleCITestCase(unittest.TestCase):
-    def test_given_test_1(self):
+class count_words_TestCase(unittest.TestCase):
+    def test_001_simple(self):
         text = "You are given a string of words"
         return_value = count_words(text)
         correct_return_value = [6,1, 31]
         self.assertEqual(return_value,correct_return_value)
-        print("test_001: testing CircleCI")
+        print("test_001: simple")
+    
+    def test_002_repeated_lower(self):
+        text = "Please write some Some unit unit tests"
+        return_value = count_words(text)
+        correct_return_value = [4, 2, 38]
+        self.assertEqual(return_value,correct_return_value)
+        print("test_002: test_repeated_lower")
 
 
 # Make the tests conveniently executable
