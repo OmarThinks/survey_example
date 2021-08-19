ffb2d918358d4b1388180608da19215b7d728fe6
Omar Magdy <contact.parmag@gmail.com>
2021-08-19 10:17:18 +0200

    test_function:add: upper and lower

--- python_task/test_functions.py
+++ python_task/test_functions.py
@@ -27,6 +27,20 @@ class count_words_TestCase(unittest.TestCase):
         self.assertEqual(return_value,correct_return_value)
         print("test_002: test_repeated_lower")
 
+    def test_003_repeated_upper(self):
+        text = "This This This is repeated upper"
+        return_value = count_words(text)
+        correct_return_value = [3, 1, 32]
+        self.assertEqual(return_value,correct_return_value)
+        print("test_003: test_repeated_upper")
+
+    def test_004_repeated_upper_and_lower(self):
+        text = "This this This this"
+        return_value = count_words(text)
+        correct_return_value = [1, 1, 19]
+        self.assertEqual(return_value,correct_return_value)
+        print("test_004: test_repeated_upper_and_lower")
+
 
 # Make the tests conveniently executable
 if __name__ == "__main__":
